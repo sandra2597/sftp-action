7c28bd2240a9105ae6e9bd47dcd8e7f11da1267d
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2023-10-03 18:10:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "<p>hello world from github actions</p>" > /app/index.html
 
-mkdir "$HOME/.ssh" && touch "$HOME/.ssh/known_hosts"
 ssh-keyscan -H ssh.strato.com > ~/.ssh/known_hosts
 echo ~/.ssh/known_hosts
+cat ~/.ssh/known_hosts
 lftp --env-password sftp://$2@$3 -e "debug; mirror -R /app /app; bye"
