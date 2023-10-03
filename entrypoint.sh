5cd5c12744412b3f8abb11c97d1064a0b6f38e28
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2023-10-03 18:17:05 +0200

    comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,10 @@ time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "<p>hello world from github actions</p>" > /app/index.html
 
+echo "writing fingerprint"
 ssh-keyscan -H ssh.strato.com > ~/.ssh/known_hosts
-echo ~/.ssh/known_hosts
+
+echo "printing fingerpint"
 cat ~/.ssh/known_hosts
+echo "Connecting"
 lftp --env-password sftp://$2@$3 -e "debug; mirror -R /app /app; bye"
