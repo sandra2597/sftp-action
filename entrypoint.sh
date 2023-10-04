be816e7c21a822621a0dcbc3c94cb41aa3437e0a
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2023-10-04 15:32:54 +0200

    Removed unused code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,8 @@
 #!/bin/sh -l
 
 echo "Variables: ${INPUT_FTP_DOMAIN} ${INPUT_FTP_HOST} ${INPUT_SOURCE}"
-echo $LFTP_PASSWORD
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-echo "<p>hello world from github actions</p>" > /app/index.html
 ls "/github/workspace/$INPUT_SOURCE"
-
-echo "writing fingerprint"
 mkdir "/root/.ssh"
 ssh-keyscan -H ssh.strato.com > /root/.ssh/known_hosts
-
-echo "printing fingerpint"
 cat /root/.ssh/known_hosts
-echo "Connecting"
-lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "debug; mirror -R /github/workspace/$INPUT_SOURCE /app; bye"
+lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "mirror -R /github/workspace/$INPUT_SOURCE /app; bye"
