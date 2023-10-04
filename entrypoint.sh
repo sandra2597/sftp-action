9739e931491aaa2245fe39d8a39006eb2510a872
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2023-10-04 15:35:43 +0200

    remove unused code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
 echo "Variables: ${INPUT_FTP_DOMAIN} ${INPUT_FTP_HOST} ${INPUT_SOURCE}"
-ls "/github/workspace/$INPUT_SOURCE"
 mkdir "/root/.ssh"
 ssh-keyscan -H ssh.strato.com > /root/.ssh/known_hosts
-cat /root/.ssh/known_hosts
 lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "mirror -R /github/workspace/$INPUT_SOURCE /app; bye"
