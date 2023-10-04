1ae614851d8457886620ed23f42350720e96bfaa
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2023-10-04 18:09:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ echo "send files"
 mkdir "/root/.ssh"
 ssh-keyscan -H ssh.strato.com > /root/.ssh/known_hosts
 
-lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "mirror -R /github/workspace/$INPUT_SOURCE/webapp.tar.gz /$INPUT_TARGET; bye"
+lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "put /github/workspace/$INPUT_SOURCE/webapp.tar.gz /$INPUT_TARGET; bye"
