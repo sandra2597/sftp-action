c970ec1353f9c248ba3130306fc768133ba5f0ed
sandra2597 <68648398+sandra2597@users.noreply.github.com>
2023-10-04 15:38:05 +0200

    add target folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Variables: ${INPUT_FTP_DOMAIN} ${INPUT_FTP_HOST} ${INPUT_SOURCE}"
 mkdir "/root/.ssh"
 ssh-keyscan -H ssh.strato.com > /root/.ssh/known_hosts
-lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "mirror -R /github/workspace/$INPUT_SOURCE /app; bye"
+lftp --env-password sftp://${INPUT_FTP_DOMAIN}@${INPUT_FTP_HOST} -e "mirror -R /github/workspace/$INPUT_SOURCE /$INPUT_TARGET; bye"
